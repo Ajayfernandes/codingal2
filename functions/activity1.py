f819da53f3ddd5cfa845840a924c87ea048995fd
AjayFernandes <ajayfernandes671@gmail.com>
2024-11-08 17:44:49 +0800

    first commit

---
+++ functions/activity1.py
@@ -0,0 +1,5 @@
+def wellwishes():
+    print("how are you?")
+    print("nice to meet you")
+
+wellwishes()
---
+++ functions/activity2.py
@@ -0,0 +1,5 @@
+def weather_conditions():
+    print("the weather is pleasant in spring")
+    print("the weather is the same in autumn")
+
+weather_conditions()
